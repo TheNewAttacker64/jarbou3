772aa612bd896b23d615faf68743f3b348050ec2
youhacker55 <57035255+youhacker55@users.noreply.github.com>
2022-02-06 17:09:05 +0000

    Update Jarbou3-pastebin.py

--- stub/Jarbou3-pastebin.py
+++ stub/Jarbou3-pastebin.py
@@ -19,8 +19,6 @@ import time
 import sqlite3
 import base64
 from urllib.request import Request, urlopen
-from scapy.all import *
-import netifaces
 
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.ciphers import (
